c41266725dca3ea88dcd51fe463f049eb87554b6
Robert <robertmcarthur1701@gmail.com>
2022-04-30 11:35:37 +0200

    Made 'enhance' button toggleable

--- myJS.js
+++ myJS.js
@@ -1,3 +1,8 @@
 function display() {
-    document.getElementById("lucas").src = "lucassat.jpg";
+    var currentImage = document.getElementById("lucas").src;
+    if (currentImage.match("lucas.jpg")) {
+        document.getElementById("lucas").src = "lucassat.jpg";
+    } else {
+        document.getElementById("lucas").src = "lucas.jpg";
+    }
 }
