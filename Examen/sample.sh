e63223bbf180432d74d339d3a89bd640bd389d4f
Aaron Kieran Southcombe <asouthcombe@cifpfbmoll.eu>
2022-05-17 12:08:11 +0200

    Exam

---
+++ Examen/sample.sh
@@ -0,0 +1,11 @@
+currentOption=0
+
+until `test $currentOption = 9`; do
+    clear
+    echo "1- Llistar el contingut del directori actual"
+    echo "2- Cercar fitxers o directoris amb el nom indicat"
+    echo "3- Crear o editar un fitxer"
+    echo "4- Comptar les paraules d'un fitxer"
+    echo "5- Protegir un fitxer"
+    echo ""
+    echo "9- Finalitzar, sortir d'aquest menu"
