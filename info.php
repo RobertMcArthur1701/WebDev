02b66ad3f31ce3f1350c5093c6f85a366ae9c238
Robert McArthur <robertmcarthur1701@gmail.com>
2022-05-18 11:09:20 +0200

    PHP stuff

--- info.php
+++ info.php
@@ -1,18 +1,19 @@
 <html>
-    <?php phpinfo();?>
     <?php
-        $dbuser = 'postgres';
-        $dbpass = '';
-        $host = 'localhost';
-        $dbname = 'myWebDB';
-        $dbh = new PDO("pgsql:host=$host;dbname=$dbname", $dbuser, $dbpass);    
-        $sql = 'SELECT * FROM example';
-        foreach ($dbh->query($sql) as $row)
-        {
-            print $row['table_id'] . " ";
-            print $row['name'] . "--";
-            print $row['email'] . "<br>";
+        function myWebDBQuery() {
+            $dbuser = 'postgres';
+            $dbpass = '';
+            $host = 'localhost';
+            $dbname = 'myWebDB';
+            $dbh = new PDO("pgsql:host=$host;dbname=$dbname", $dbuser, $dbpass);    
+            $sql = 'SELECT * FROM example';
+            foreach ($dbh->query($sql) as $row)
+            {
+                print $row['table_id'] . " ";
+                print $row['name'] . "--";
+                print $row['email'] . "<br>";
+            }
         }
-        
+        myWebDBQuery();
     ?>
 </html>
