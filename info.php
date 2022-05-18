d8bd0b049d62f2231ac4ff0566f20b49b883654c
Robert McArthur <robertmcarthur1701@gmail.com>
2022-05-18 11:01:01 +0200

    PHP stuff

--- info.php
+++ info.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Info</title>
-    <script src="functions.js"></script>
-</head>
-<body>
-    <h1>Testtext</h1>
     <?php phpinfo();?>
     <?php
         $dbuser = 'postgres';
@@ -27,5 +17,4 @@
         }
         tableQuery();
     ?>
-</body>
 </html>
